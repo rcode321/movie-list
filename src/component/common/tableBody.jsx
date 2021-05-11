2897da48ebf7cd3007920285267593277a0769f9
Raf <rafaelmendoza004@gmail.com>
2021-05-12 04:41:19 +0800

    create a tableHeader component

---
+++ src/component/common/tableBody.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class TableBody extends Component {
+  state = {};
+  render() {
+    return (
+      <tbod>
+        <tr>
+          <td></td>
+        </tr>
+      </tbod>
+    );
+  }
+}
+
+export default TableBody;
